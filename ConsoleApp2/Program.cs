28fd79cca7b5a2c5a40c0ad1f61eaca44bd0f638
andreimuresan88 <andreimuresan88@gmail.com>
2021-12-10 10:37:20 +0200

    added if/while/for/switch ex

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp2
 
             const int MAX_SIZE = 100;
             const double PI = 3.1415;
+            const int MAX_NUMBER = 5;
 
             Console.WriteLine("Hello World!");
 
@@ -60,6 +61,149 @@ namespace ConsoleApp2
 
             Console.WriteLine(typeof(int).IsPrimitive);
             Console.WriteLine("Hello World!");
+
+            int[] list = { 1, 3, 4, 6 };
+            for (int j = 0; j < list.Length; j++)
+            {
+                Console.Write(list[j] + " ");
+            }
+
+            int number = 53;
+            //int number = -100;
+            //int number = 0;
+            if (number >= 0)
+            {
+                Console.WriteLine("number is positive");
+            }
+            else
+            {
+                Console.WriteLine("number is negative");
+            }
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("evan number!");
+            }
+            else
+            {
+                Console.WriteLine("odd number");
+            }
+
+            if (number <= 40 && number >= 0)
+            {
+                if (number <= 20)
+                {
+                    Console.WriteLine("Student failed");
+                }
+                else
+                {
+                    Console.WriteLine("student passed");
+                }
+            }
+            else
+            {
+                Console.WriteLine("number si invalid");
+            }
+            //one line if/else
+            Console.WriteLine((number % 2 == 0 ? "even" : "odd"));
+
+            if (number == 1)
+            {
+                Console.WriteLine("Monday");
+            }
+            if (number == 2)
+            {
+                Console.WriteLine("Tuesday");
+            }
+            if (number == 3)
+            {
+                Console.WriteLine("Wedneday");
+            }
+            if (number == 4)
+            {
+                Console.WriteLine("Thurday");
+            }
+            if (number == 5)
+            {
+                Console.WriteLine("Friday");
+            }
+            if (number == 6)
+            {
+                Console.WriteLine("Saturday");
+            }
+            if (number == 7)
+            {
+                Console.WriteLine("Sunday");
+            }
+            if (number > 7 || number < 0)
+            {
+                Console.WriteLine("invalid number");
+            }
+
+            switch (number)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Monday");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Tuesday");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Wedneday");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Thurday");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Friday");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("Saturday");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("Sunday");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("invalid number");
+                        break;
+                    }
+            }
+
+            int counter = 0;
+            while (counter <= MAX_NUMBER)
+            {
+                Console.WriteLine("Current number is: " + counter);
+                Console.WriteLine("Current number is: " + counter.ToString());
+                counter++;
+            }
+
+            counter = 0;
+            do
+            {
+                Console.WriteLine("Current number is: " + counter);
+                counter++;
+            } while (counter <= MAX_NUMBER);
+
+            for (counter = 0; counter < MAX_NUMBER; counter++)
+            {
+                Console.WriteLine("Counter number is: {0}", counter);
+            }
         }
+
     }
 }
